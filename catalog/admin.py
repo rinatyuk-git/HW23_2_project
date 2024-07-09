fd73958534011761f32c67c5e1d3e099193d5b23
rinatyuk-git <17_ufo_RB@proton.me>
2024-07-09 13:20:10 -0400

    create admin.py

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
+from catalog.models import Product, Category
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("id", "product_name", "product_price", "category",)
+# продуктов выведите в список id, название, цену и категорию.
+    list_filter = ("category",)
+# результат отображения фильтровать по категории
+    search_fields = ("product_name", "product_info",)
+# осуществлять поиск по названию и полю описания.
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("id", "category_name",)
+# Для категорий выведите id и наименование в список отображения
+
+
+# admin.site.register(Product)
 # Register your models here.
