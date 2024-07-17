0989ee2d7ce839910863c6be133a4d7f54ad2d7e
rinatyuk-git <17_ufo_RB@proton.me>
2024-07-17 11:21:40 -0400

    update setups in urls & settings

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
-from catalog.views import home, contacts
+from catalog.views import home, contacts, product_view, product_detail
+from catalog.apps import CatalogConfig
+
+app_name = CatalogConfig.name
 
 urlpatterns = [
 	path('', home, name='home'),
-	path('contacts/', contacts, name='contacts')
+	path('contacts/', contacts, name='contacts'),
+	path('product_view/', product_view, name='product_view'),
+	path('catalog/<int:pk>/', product_detail, name='product_detail')
 ]
 
 # from catalog.apps import CatalogConfig
--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from catalog.models import Product
 
 
 def home(request):
@@ -12,3 +14,16 @@ def contacts(request):
         message = request.POST.get('message')
         print(f'{name} ({phone}): {message}')
     return render(request, 'catalog/contacts.html')
+
+
+def product_view(request):
+    products = Product.objects.all()
+    context = {'products': products}
+    return render(request, 'catalog/product_basis_form.html', context)
+
+
+def product_detail(request, pk):
+    # product = Product.objects.get(pk=pk)
+    product = get_object_or_404(Product, pk=pk)
+    context = {'product': product}
+    return render(request, 'catalog/product_detail.html', context)
