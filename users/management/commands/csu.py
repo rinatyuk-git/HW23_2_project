d9f27928fcedbbcb8265c114102c684c546538d2
rinatyuk-git <17_ufo_RB@proton.me>
2024-08-16 13:10:12 -0400

    superuser setup

---
+++ users/management/commands/csu.py
@@ -0,0 +1,19 @@
+from django.core.management import BaseCommand
+
+from users.models import User
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        user = User.objects.create(
+            email='admin@admin.admin',
+            first_name='Admin',
+            last_name='Admins',
+            is_active=True,
+            is_staff=True,
+            is_superuser=True,
+        )
+
+        user.set_password('123456admin')
+        user.save()
